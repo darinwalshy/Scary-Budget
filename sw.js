43b360826c7ccbabae49ba0583d46146b36abfcc
darinwalshy <darinwalshy@gmail.com>
2026-07-17 11:39:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 🛠️ UNIQUE IDENTIFIERS FOR THIS APP
 // ==========================================
-const APP_PREFIX = 'scary_budget_v1_7_';
+const APP_PREFIX = 'scary_budget_v1_8_';
 const CACHE_NAME = APP_PREFIX + 'cache';
 
 const ASSETS = [
